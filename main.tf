8595c3303d40f8dfae14b2bf9a432c9d1202ab16
Claude Eke <visionary_p@yahoo.com>
2024-03-20 13:12:57 -0700

    created new files

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Authenticate Terraform with AWS by configuring the AWS Provider
+provider "aws" {
+  region = "us-west-1"
+  profile = "terraform-user"
+}
+
+# Stores the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket = "claude-terraform-remote-state"
+    key    = "terraform.tfstate.dev"
+    region = "us-west-1"
+    profile = "terraform-user"
+  }
+}
