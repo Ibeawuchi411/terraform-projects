680fe53bfe0da53e8e5099b707c36d345486a9eb
Claude Eke <visionary_p@yahoo.com>
2024-03-23 13:10:15 -0700

    created sns file

--- variable.tf
+++ variable.tf
@@ -85,3 +85,10 @@ variable "ssl_certificate_arn" {
   description   = "ssl certficate arn"
   type          = string
 }
+
+# sns topic variable
+variable "operator_email" {
+  default       = "claude.eke77@gmail.com"
+  description   = "a valid email address"
+  type          = string
+}
